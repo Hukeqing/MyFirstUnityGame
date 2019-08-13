a3c3ae99fe8544cfbfcc7f3b124829c320dd09de
HuKeqing <keqing.hu@icloud.com>
2019-08-13 23:19:37 +0800

    7

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    void Update()
+    {
+        if (transform.position.y < 0)
+        {
+            Player.cnt--;
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public float MoveSpeed;
+    public GameObject Shell;
+    public float Force;
+
+    public Text leaveNum;
+    public GameObject GameOver;
+
+    public Transform FirePoint;
+
+    public static int cnt = 36;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(transform.forward * MoveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(-transform.forward * MoveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-transform.right * MoveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(transform.right * MoveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            transform.Translate(-transform.up * MoveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(transform.up * MoveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject newShell = Instantiate(Shell, FirePoint.position, FirePoint.rotation);
+            newShell.GetComponent<Rigidbody>().AddForce(transform.forward * Force);
+            Destroy(newShell, 3);
+        }
+        leaveNum.text = "剩余方块：" + cnt.ToString();
+        if (cnt == 0)
+        {
+            leaveNum.gameObject.SetActive(false);
+            GameOver.SetActive(true);
+        }
+    }
+}
